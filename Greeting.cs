c513a57522103de2f8d479b4fe0f97f6820e1d00
z003u7sk <utkarsh.joshi@siemens-healthineers.com>
2019-08-02 11:38:03 +0530

    Greeting

---
+++ Greeting.cs
@@ -0,0 +1,5 @@
+public class Greeting{
+	public string greeting(){
+		return "Namaskara";
+	}
+}
--- SuperMath.cs
+++ SuperMath.cs
@@ -2,4 +2,8 @@ public class SuperMath {
 	public int Sum(int i, int j){
 		return i+j;
 	}
+	
+	public int Product(int i,int j){
+		return i*j;
+	}
 }
